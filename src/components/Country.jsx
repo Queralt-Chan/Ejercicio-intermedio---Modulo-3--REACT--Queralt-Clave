14598fcfd7697711e324aabbb3d5c5907485c79d
queralt-chan <queraltbali@gmail.com>
2024-02-06 11:06:29 +0800

    first commit

---
+++ src/components/Country.jsx
@@ -0,0 +1,14 @@
+function Country () {
+return (
+
+    <div>
+        <img src="" alt="" />
+        <h2>Pais</h2>
+        <h4>Capital</h4>
+        <h4>Continente</h4>
+    </div>
+)
+
+}
+
+export default Country;
---
+++ src/components/ListCountries.jsx
@@ -0,0 +1,12 @@
+
+
+function ListCountries ({list}) {
+    console.log (list)
+
+return (
+  <div></div>
+
+)
+
+}
+export default ListCountries;
